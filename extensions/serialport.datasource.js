acd3ff9aba10c37b3c6db601766d1f24e4e5c713
3sigma <n.gachadoit@infonie.fr>
2016-06-24 16:40:11 +0200

    Removed extra console.log

--- extensions/serialport.datasource.js
+++ extensions/serialport.datasource.js
@@ -7,7 +7,6 @@
 		var self = this;
 		var currentSettings = settings;
 		var com = require('serialport');
-		//console.log("isOpen debut: ", isOpen);
 		var refreshInterval;
 		var lastSentTime = 0;
 		var currentTime = new Date();
@@ -34,9 +33,7 @@
 				console.log("JSON.parse(msg): ", JSON.parse(msg));
 				// Extract variable name from, for example,
 				// currentSettings.value = [datasources["datasourcename"]["variablename1"], datasources["datasourcename"]["variablename2"]]
-				//console.log("msg 2: ", (_.keys(JSON.parse(msg)[0])).split('"')[3]);
 				var variableName = (_.keys(JSON.parse(msg))[0]).split('"')[3];
-				console.log("variableName: ", variableName);
 				var currentObj = {};
 				currentObj[variableName] = _.values(JSON.parse(msg))[0];
 			
@@ -55,11 +52,9 @@
 						}
 					}
 				}
-				//$.extend(dataObj, {'_crc': crcValue});
 				
 				// Convert into a string of values
 				dataToSend = "";
-				console.log("currentSettings.separator: ", currentSettings.separator);
 				for (var i = 0; i < listVariablesToSend.length; i++) {
 					dataToSend += _.isUndefined(dataObj[listVariablesToSend[i]]) ? "0" + currentSettings.separator : dataObj[listVariablesToSend[i]] + currentSettings.separator;
 				}
@@ -68,25 +63,15 @@
 				// Very important ! Don't forget
 				dataToSend += "\r";
 				
-				console.log("listVariablesToSend: ", listVariablesToSend);
-				console.log("dataObj: ", dataObj);
-				console.log("dataToSend: ", dataToSend);
-				
 				// When data arrives (from a slider, for example), send it
 				// "immediately" (not faster than every 100 ms)
-				console.log("dataToSend immediate: ", dataToSend);
 				currentTime = new Date();
 				if ((currentTime - lastSentTime) > 100) {
 					lastSentTime = currentTime;
-					//console.log("isOpen immediate: ", isOpen);
 					if (isOpen) {
-						console.log("dataToSend serialPort immediate: ", dataToSend);
 						serialPort.write(dataToSend, function (error) {
 							if (error) {
 						    	console.log("Error writing to the serial port immediate: ", error);
-						    }
-						    else {
-						    	console.log("Sending data immediate: ", dataToSend);
 						    }
 						});
 					}
@@ -95,7 +80,6 @@
 			
 			socket.on('disconnect', function () {
 				console.log("Serial port socket disconnected");
-				//clearInterval(refreshInterval);
 			});
 			socket.on('error', function (error) {
 				console.log("Socket error: ", error);
@@ -111,7 +95,6 @@
 					} 
 					else {
 						isOpen = false;
-						//console.log("isOpen discardSerialport: ", isOpen);
 					}
 				});			
 			}
@@ -127,7 +110,6 @@
 
 		function initializeDataSource() {
 			
-			console.log("refreshIntervals: ", refreshIntervals);
 			for (r in refreshIntervals) {
 			    clearInterval(refreshIntervals[r]);
 			}
@@ -139,7 +121,6 @@
 	        refreshInterval = setInterval(
 	        	function(){
 					// Write data to the serial port
-					//console.log("isOpen refreshInterval: ", isOpen);
 					currentTime = new Date();
 					if ((isOpen) && ((currentTime - lastSentTime) > 100)) {
 						console.log("dataToSend serialPort: ", dataToSend);
@@ -147,32 +128,22 @@
 							if (error) {
 						    	console.log("Error writing to the serial port: ", error);
 						    }
-						    else {
-						    	console.log("Sending data: ", dataToSend);
-						    }
 						});
 					}
 	        	},
 	        	(_.isUndefined(currentSettings.refresh_rate) ? 1000 : currentSettings.refresh_rate)
 	        );
 	        refreshIntervals.push(refreshInterval);
-		       
-			/*if (socket) {
-				console.log("Disconnect socket");
-				socket.disconnect();
-			}*/
 			
 			// Reset connection to Serial port
 			discardSerialport();
 			
 			console.log("Open port");
 			isOpen = true; // Needed to avoid a double open by updateNow
-			//console.log("isOpen initializeDataSource: ", isOpen);
 			connectToSerialport(currentSettings.port, currentSettings.baudrate);
 			
 		    serialPort.on('open',function() {
 		    	isOpen = true;
-		    	//console.log("isOpen initializeDataSource serialPort.on('open': ", isOpen);
 		        console.log('Port open');
 		    });
 		
@@ -182,34 +153,19 @@
 			    	
 			    	var arrData = "[" + data + "]";
 			    	var listData = JSON.parse(arrData);
-			    	// Conduit à ça, le plus efficace mais génère une erreur sur la ligne
-			    	// précédente s'il y a une chaine parmi les valeurs numériques
-			    	//listData = [ 1834.93, 0, 0, 0, 0, 0 ];
-			    	
-			    	//var listData = data.split(",");
-			    	// Conduit à ça, beaucoup moins efficace mais ne génère pas d'erreur s
-			    	// s'il y a une chaine parmi les valeurs numériques
-			    	//listData = [ '1834.93', '0.00', '0.00', '0.00', '0.00', '0.00' ];
 			    	
 			    	// Add raw data to the object
 			    	listData.push(data);
-			    	
-			    	//console.log("");
-			        //console.log("Received data: ", listData);
-			        //console.log("");
 			        
 			        // Create JSON object from the list of variables to read
 			        newData = _.object(listVariablesToRead, listData);
 			        
 			        // Add the variables to send
-			        //console.log("newDataToSend: ", newDataToSend);
 			        $.extend(newData, newDataToSend);
-			        //console.log("New data: ", newData);
 			        
 			        updateCallback(newData);
 			    }
 			    catch(e) {
-			    	console.log(data);
 			        console.log("Parse error: ", e); //error in the above string
 			    }
 	        		            
@@ -218,21 +174,17 @@
 		    serialPort.on('close',function() {
 		        console.log('Port closed');
 		        isOpen = false;
-		        //console.log("isOpen initializeDataSource serialPort.on('close': ", isOpen);
 		    });
 		
 		    serialPort.on('error',function(err) {
 		        console.log('Error: ', err.message);
 		        alert('Error: ' + err.message);
 		        isOpen = false;
-		        //console.log("isOpen initializeDataSource serialPort.on('error': ", isOpen);
 		    });
 		
 		}
 
 		this.updateNow = function () {
-			//console.log(isOpen);
-			//console.log("isOpen updateNow: ", isOpen);
 			if (!isOpen) {
 				initializeDataSource();
 			}
@@ -246,14 +198,12 @@
 		this.onDispose = function () {
 			// Stop responding to messages
 		    serialPort.on('data', function(data) {
-		        // On ne fait plus rien 
+		        // We do nothing 
 		    });
 			discardSerialport();
 		};
 
 		this.onSettingsChanged = function (newSettings) {
-			console.log("currentSettings.separator: ", currentSettings.separator);
-			console.log("newSettings.separator: ", newSettings.separator);
             if ((newSettings.port != currentSettings.port)
             	|| (newSettings.baudrate != currentSettings.baudrate)) {
             	initializeDataSource();
